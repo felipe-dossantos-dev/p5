2b813c47fc1f2a895be4063910a0f9c036bd0f19
Felipe Santos <santos.flip@gmail.com>
2024-04-10 16:13:04 -0300

    formatando

--- sketch.js
+++ sketch.js
@@ -1,70 +1,25 @@
-class FractalBox {
-
-  constructor(x, y, z, size) {
-    this.position = createVector(x, y, z)
-    this.size = size;
-    this.children = [];
-  }
-
-  show() {
-    push();
-    // stroke(255);
-    fill(255);
-    translate(this.position);
-    box(this.size);
-    pop();
-  }
-
-  generate() {
-    var boxes = []
-    for (let x = -1; x < 2; x++) {
-      for (let y = -1; y < 2; y++) {
-        for (let z = -1; z < 2; z++) {
-          const sum = abs(x) + abs(y) + abs(z)
-          if (sum > 1) 
-          {
-            var newSize = this.size / 3;
-            var newBox = new FractalBox(
-              this.position.x + x * newSize, 
-              this.position.y + y * newSize, 
-              this.position.z + z * newSize,
-              newSize);
-            boxes.push(newBox);
-          }
-        }
-      }
-    }
-    return boxes;
-  }
-}
-
-var fractal;
-var rotateAngle = 1;
-var sponge = [];
-
 function setup() {
-  createCanvas(600, 600, WEBGL);
-  fractal = new FractalBox(0, 0, 0, 100);
-  sponge.push(fractal);
-}
-
-function mousePressed() {
-  var next = []
-  for (const box of sponge) {
-    next = next.concat(box.generate())
-  }
-  sponge = next;
+  createCanvas(600, 600, P2D);
+  background(0);
 }
 
 function draw() {
-  background(51);
-
-  rotateX(rotateAngle);
-  rotateY(rotateAngle);
-
-  for (const box of sponge) {
-    box.show();
+  loadPixels();
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
+      var a = map(x, 0, width, -2.5, 1.0);
+      var b = map(y, 0, height, -1.0, 1.0);
+      var ca = a;
+      var cb = b;
+      var n = 0;
+      while (abs(a + b) < 4 && n < 255) {
+        var tempA = a * a - b * b + ca;
+        b = 2 * a * b + cb;
+        a = tempA;
+        n++;
+      }
+      pixels[x + y * width] = color(n * 10 % 255, n * 5 % 255, n * 3 % 255);
+    }
   }
-
-  rotateAngle += 0.01;
+  updatePixels();
 }
